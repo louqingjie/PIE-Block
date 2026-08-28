b6bbad4919a33d16a14da164a4606d89cd00b995
娄庆杰 <louqingjie@outlook.com>
2026-08-28 21:34:01 +0800

    chore: 删除 Godot 相关代码与产物

--- scripts/_dbg_icon.gd
+++
@@ -1,5 +0,0 @@
-@tool
-extends EditorScript
-func _run() -> void:
-	print("ICON=", ProjectSettings.get_setting("application/config/icon"))
-	print("SPLASH=", ProjectSettings.get_setting("application/boot_splash/image"))
--- scripts/dev_deploy.gd
+++
@@ -1,9 +0,0 @@
-extends SceneTree
-
-func _initialize() -> void:
-	var tc = load("res://scripts/toolchain.gd").new()
-	if tc.ensure_deployed():
-		print("Toolchain deployed OK")
-	else:
-		print("Toolchain deployment FAILED")
-	quit(0)
